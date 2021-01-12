3a2baa2e94e1641bdb072bf8aaad6c316f07719d
medmoe <med.seffah@gmail.com>
2021-01-12 00:11:47 -0500

    upload increment decrement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
 import './App.css';
 
+class IncDec extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      num: props.num
+    }
+    this.inc = this.clickHandler.bind(this);
+  }
+  clickHandler(x){
+    let value = this.state.num + x;
+    if(value < 0){
+      alert("Cannot be less than zero");
+    }else{
+      this.setState({
+        num: value
+      })
+    }
+  }
+  render(){
+    return <div className="container">
+      <div className="output">
+        {this.state.num}
+      </div>
+      <div className="buttons">
+        <button onClick={() => this.clickHandler(1)}>increment</button>
+        <button onClick={() => this.clickHandler(-1)}>decrement</button>
+      </div>
+      
+    </div>
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IncDec num = {5} />
   );
+}
+IncDec.propTypes = {
+  num: PropTypes.number
 }
 
 export default App;
